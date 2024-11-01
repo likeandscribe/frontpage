ee69885cbdf83f3ce56d60926b19cac82e50ab97
Tom Sherman <the.tomsherman@gmail.com>
2024-11-01 11:45:51 +0000

    Detect AT URI from http and html alternate links

--- packages/atproto-browser/app/actions.ts
+++ packages/atproto-browser/app/actions.ts
@@ -3,25 +3,86 @@
 import { getAtUriPath } from "@/lib/util";
 import { AtUri, isValidHandle } from "@atproto/syntax";
 import { redirect } from "next/navigation";
+import { parse as parseHtml } from "node-html-parser";
 
 export async function navigateUri(_state: unknown, formData: FormData) {
   const uriInput = formData.get("uri") as string;
+
   const handle = parseHandle(uriInput);
 
   if (handle) {
     redirect(getAtUriPath(new AtUri(`at://${handle}`)));
   }
 
-  let uri;
+  const result =
+    uriInput.startsWith("http://") || uriInput.startsWith("https://")
+      ? await getAtUriFromHttp(uriInput)
+      : parseUri(uriInput);
+
+  if ("error" in result) {
+    return result;
+  }
+
+  redirect(getAtUriPath(result.uri));
+}
+
+type UriParseResult =
+  | {
+      error: string;
+    }
+  | { uri: AtUri };
+
+async function getAtUriFromHttp(url: string): Promise<UriParseResult> {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return { error: `Failed to fetch ${url}` };
+  }
+
+  const linkHeader = response.headers.get("Link");
+  if (linkHeader) {
+    const atUriMatch = linkHeader.match(/<(at:\/\/[^>]+)>; rel="alternate"/);
+    if (atUriMatch && atUriMatch[1]) {
+      const result = parseUri(atUriMatch[1]);
+      if ("uri" in result) {
+        return result;
+      }
+    }
+  }
+
+  const html = await response.text();
+  let doc;
   try {
-    uri = new AtUri(uriInput);
+    doc = parseHtml(html);
   } catch (_) {
     return {
-      error: `Invalid URI: ${uriInput}`,
+      error: `Failed to parse HTML from ${url}`,
     };
   }
 
-  redirect(getAtUriPath(uri));
+  const alternates = doc.querySelectorAll('link[rel="alternate"]');
+  const atUriAlternate = alternates.find((link) =>
+    link.getAttribute("href")?.startsWith("at://"),
+  );
+  if (atUriAlternate) {
+    const result = parseUri(atUriAlternate.getAttribute("href")!);
+    if ("uri" in result) {
+      return result;
+    }
+  }
+
+  return {
+    error: `No AT URI found in ${url}`,
+  };
+}
+
+function parseUri(input: string): UriParseResult {
+  try {
+    return { uri: new AtUri(input) };
+  } catch (_) {
+    return {
+      error: `Invalid URI: ${input}`,
+    };
+  }
 }
 
 function parseHandle(input: string): string | null {
