c38def65b356aa92dd010d10ec61d27145f7e3e8
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2024-11-06 20:12:33 +0000

    removed class from theme-toggle and added to base component

--- packages/frontpage/app/(app)/_components/theme-toggle.tsx
+++ packages/frontpage/app/(app)/_components/theme-toggle.tsx
@@ -18,24 +18,15 @@ export function ThemeToggleMenuGroup() {
       <DropdownMenuLabel className="truncate">Theme</DropdownMenuLabel>
       <DropdownMenuSeparator />
       <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
-        <DropdownMenuRadioItem
-          value="light"
-          className="flex gap-1.5 cursor-pointer"
-        >
+        <DropdownMenuRadioItem value="light" className="flex gap-1.5">
           <SunIcon className="size-4" />
           <span>Light</span>
         </DropdownMenuRadioItem>
-        <DropdownMenuRadioItem
-          value="dark"
-          className="flex gap-1.5 cursor-pointer"
-        >
+        <DropdownMenuRadioItem value="dark" className="flex gap-1.5">
           <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
           <span>Dark</span>
         </DropdownMenuRadioItem>
-        <DropdownMenuRadioItem
-          value="system"
-          className="flex gap-1.5 cursor-pointer"
-        >
+        <DropdownMenuRadioItem value="system" className="flex gap-1.5">
           <Half2Icon className="h-[1.2rem] w-[1.2rem]" />
           <span>System</span>
         </DropdownMenuRadioItem>
