db8a800ecf5360ed7b498548edc8f13499436a19
Damein Sedgwick <damienksedgwick@gmail.com>
2024-11-15 11:08:06 +0000

    added comments icon

--- packages/frontpage/app/(app)/_components/post-card.tsx
+++ packages/frontpage/app/(app)/_components/post-card.tsx
@@ -95,7 +95,7 @@ export async function PostCard({
         </h2>
       </div>
 
-      <div className="flex items-center gap-6">
+      <div className="pr-2 flex items-center gap-6">
         <VoteButton
           voteAction={async () => {
             "use server";
@@ -127,8 +127,25 @@ export async function PostCard({
           }
           votes={votes}
         />
-        <Link href={postHref} className="hover:underline">
-          {commentCount} comments
+        <Link
+          href={postHref}
+          className="hover:underline flex items-center gap-1.5"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+            />
+          </svg>
+          {commentCount}
         </Link>
       </div>
     </article>
