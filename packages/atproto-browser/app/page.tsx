89bed0815ec51350870f0b7291df9b487ba0ccb8
Tom Sherman <tom@sherman.is>
2025-03-07 17:31:26 +0000

    Add supported inputs

--- packages/atproto-browser/app/page.tsx
+++ packages/atproto-browser/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "@/lib/link";
 import { AtUriForm } from "./aturi-form";
 import type { Metadata } from "next";
-import { EXAMPLE_PATH } from "@/app/consts";
 
 export const metadata: Metadata = {
   title: "ATProto Browser",
@@ -16,8 +15,49 @@ export default function Home() {
         <AtUriForm />
       </div>
       <p>
-        eg. <Link href={`/at/${EXAMPLE_PATH}`}>at://{EXAMPLE_PATH}</Link>
+        eg.{" "}
+        <Link href="/at/did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.like/3kyutnrmg3s2r">
+          at://did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.like/3kyutnrmg3s2r
+        </Link>
       </p>
+
+      <h2 style={{ marginTop: 100 }}>Supported inputs</h2>
+
+      <ul>
+        <li>
+          at:// URIs eg.{" "}
+          <Link href="/at/did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.like/3kyutnrmg3s2r">
+            at://did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.like/3kyutnrmg3s2r
+          </Link>
+        </li>
+        <li>
+          The at:// is optional eg.{" "}
+          <code>
+            did:plc:2xau7wbgdq4phuou2ypwuen7/app.bsky.feed.like/3kyutnrmg3s2r
+          </code>{" "}
+          or <code>did:plc:2xau7wbgdq4phuou2ypwuen7</code> will work too.
+        </li>
+        <li>
+          Handles eg. <Link href="/at/tom.sherman.is">@tom.sherman.is</Link>.
+          Note, the &apos;@&apos; is optional.
+        </li>
+        <li>
+          Bluesky posts and profile HTTPS URLs (when the author has indexing
+          enabled)
+        </li>
+        <li>
+          HTTP(s) URLs where the HTML contains a{" "}
+          <code>
+            {"<"}link rel=&quot;alternate&quot;{">"} with an at:// URI in the{" "}
+            <code>href</code> attribute
+          </code>
+        </li>
+        <li>
+          lex URIs eg. <code>lex:app.bsky.feed.like</code>
+        </li>
+      </ul>
+
+      <hr />
 
       <footer>
         <p>
