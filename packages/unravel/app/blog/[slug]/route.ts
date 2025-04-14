eb214daf0ce4357f2e472aef2cae414ff9fe6814
Tom Sherman <tom@sherman.is>
2025-04-14 11:41:57 +0100

    Fix error

--- packages/unravel/app/blog/[slug]/route.ts
+++ packages/unravel/app/blog/[slug]/route.ts
@@ -1,11 +1,11 @@
-export function GET(
+export async function GET(
   request: Request,
-  { params: { slug } }: { params: { slug: string } },
+  { params }: { params: Promise<{ slug: string }> },
 ) {
   return new Response(null, {
     status: 308,
     headers: {
-      Location: `https://frontpage.fyi/blog/${slug}`,
+      Location: `https://frontpage.fyi/blog/${(await params).slug}`,
     },
   });
 }
