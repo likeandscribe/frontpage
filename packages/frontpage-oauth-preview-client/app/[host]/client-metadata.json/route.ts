8c306da4422ed873152bdedb4b4ef31fa9f76fb8
Tom Sherman <tom@sherman.is>
2025-04-25 20:46:26 +0100

    Add logging

--- packages/frontpage-oauth-preview-client/app/[host]/client-metadata.json/route.ts
+++ packages/frontpage-oauth-preview-client/app/[host]/client-metadata.json/route.ts
@@ -14,22 +14,38 @@ export async function GET(
   const vercel = new Vercel({
     bearerToken: process.env.VERCEL_TOKEN,
   });
-  const currentDeployment = await vercel.deployments.getDeployment({
-    idOrUrl: process.env.VERCEL_DEPLOYMENT_ID!,
-  });
+  const currentDeployment = await vercel.deployments
+    .getDeployment({
+      idOrUrl: process.env.VERCEL_DEPLOYMENT_ID!,
+    })
+    .catch(() => null);
+
+  if (!currentDeployment) {
+    console.log("No current deployment found for", {
+      idOrUrl: process.env.VERCEL_DEPLOYMENT_ID!,
+    });
+    return new Response("Not found", { status: 404 });
+  }
 
   const team = currentDeployment.team; // Get likeandscribe team data from current deployment. Avoids having to hardcode team ID.
 
   if (!team) {
+    console.log("No team found");
     return new Response("Not found", { status: 404 });
   }
 
-  const deployment = await vercel.deployments.getDeployment({
-    teamId: team.id,
-    idOrUrl: host,
-  });
+  const deployment = await vercel.deployments
+    .getDeployment({
+      teamId: team.id,
+      idOrUrl: host,
+    })
+    .catch(() => null);
 
   if (!deployment) {
+    console.log("No deployment found for", {
+      teamId: team.id,
+      idOrUrl: host,
+    });
     return new Response("Not found", { status: 404 });
   }
 
