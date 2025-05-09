fc6d28815a57a033cd7d2da36a092adc525d1f5c
Tom Sherman <the.tomsherman@gmail.com>
2025-05-09 12:08:40 +0100

    Nicer skeletons

--- packages/frontpage/app/(app)/post/[postAuthor]/[postRkey]/page.tsx
+++ packages/frontpage/app/(app)/post/[postAuthor]/[postRkey]/page.tsx
@@ -8,6 +8,7 @@ import { LinkAlternateAtUri } from "@/lib/components/link-alternate-at";
 import { nsids } from "@/lib/data/atproto/repo";
 import { Suspense } from "react";
 import { Skeleton } from "@/lib/components/ui/skeleton";
+import clsx from "clsx";
 
 export async function generateMetadata(props: {
   params: Promise<PostPageParams>;
@@ -77,10 +78,27 @@ export default async function Post(props: { params: Promise<PostPageParams> }) {
 function CommentTreeFallback() {
   return (
     <div className="flex flex-col gap-6">
-      {Array.from({ length: 3 }).map((_, i) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <Skeleton key={i} className="h-12 w-full" />
-      ))}
+      <CommentSkeleton />
+      <CommentSkeleton className="pl-8" />
+      <CommentSkeleton />
+    </div>
+  );
+}
+
+function CommentSkeleton({ className }: { className?: string }) {
+  return (
+    <div className={clsx("flex flex-col gap-2", className)}>
+      <div className="flex items-center gap-2">
+        <Skeleton className="h-6 w-6 rounded-full" />
+        <Skeleton className="h-4 w-24" />
+        <Skeleton className="h-3 w-16" />
+      </div>
+      <Skeleton className="h-16 w-full" />
+      <div className="flex items-center gap-4 mt-1">
+        <Skeleton className="h-4 w-4" />
+        <Skeleton className="h-4 w-4" />
+        <Skeleton className="h-4 w-4" />
+      </div>
     </div>
   );
 }
