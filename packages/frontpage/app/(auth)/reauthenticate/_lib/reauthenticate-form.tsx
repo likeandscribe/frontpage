f25b65bbb437a50e2e20097c70a2b32e6c90ed81
Tom Sherman <tom@sherman.is>
2025-07-29 14:56:19 +0100

    Add avatar

--- packages/frontpage/app/(auth)/reauthenticate/_lib/reauthenticate-form.tsx
+++ packages/frontpage/app/(auth)/reauthenticate/_lib/reauthenticate-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useActionState } from "react";
+import { type ReactNode, useActionState } from "react";
 import { reauthenticateAction } from "./reauthenticate-action";
 import { Button } from "@/lib/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/lib/components/ui/alert";
@@ -10,15 +10,18 @@ export function ReauthenticateForm({
   // TODO: Use this prop to redirect after re-authentication, requires changes in signIn method
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   redirectPath,
+  avatar,
 }: {
   redirectPath?: string;
+  avatar: ReactNode;
 }) {
   const [state, action, isPending] = useActionState(reauthenticateAction, null);
 
   return (
     <div className="space-y-3">
-      <form action={action}>
-        <Button className="w-full" type="submit" disabled={isPending} size="lg">
+      <form action={action} className="flex gap-2 items-center">
+        <div>{avatar}</div>
+        <Button type="submit" disabled={isPending} size="lg" className="w-full">
           Re-authenticate now
         </Button>
       </form>
