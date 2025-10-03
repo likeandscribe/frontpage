b9bf60275b370973a82fd7dc797f5b9030de70b5
Tom Sherman <tom@sherman.is>
2025-10-03 22:37:08 +0100

    Implement reauthenticate UI

---
+++ packages/frontpage/app/(auth)/reauthenticate/page.tsx
@@ -0,0 +1,60 @@
+import { getSession, signOut } from "@/lib/auth";
+import { AUTH_SCOPES } from "@repo/frontpage-oauth";
+import { redirect } from "next/navigation";
+import { ReauthenticateForm } from "./_lib/reauthenticate-form";
+import { Button } from "@/lib/components/ui/button";
+import { revalidatePath } from "next/cache";
+import { UserAvatar } from "@/lib/components/user-avatar";
+
+export default async function LoginPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ redirect?: string }>;
+}) {
+  const session = await getSession();
+  if (!session) {
+    redirect("/login?error=You've been logged out. Please log in again.");
+  }
+
+  const redirectParam = (await searchParams).redirect;
+
+  // TODO: Test this doesnt allow you to redirect to an external URL
+  const redirectPath = redirectParam?.startsWith("/") ? redirectParam : "/";
+
+  if (session.user.scope === AUTH_SCOPES) {
+    console.warn(
+      "User has AUTH_SCOPES, redirecting to the specified path or defaulting to /",
+    );
+    redirect(redirectPath);
+  }
+
+  return (
+    <>
+      <div className="text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100">
+          Re-authenticate to Frontpage
+        </h2>
+        <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+          You need to re-authenticate to continue using Frontpage so that we
+          have the latest permissions to access your data.
+        </p>
+      </div>
+      <div>
+        <ReauthenticateForm
+          avatar={<UserAvatar did={session.user.did} size="smedium" />}
+        />
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+            revalidatePath("/", "layout");
+          }}
+        >
+          <Button size="lg" variant="secondary" className="w-full mt-4">
+            Logout
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+}
